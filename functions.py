c9b7a2449e70e5e934a53ebc43f1fdca81a47440
Ling Fei Zhang <lzhang133@gmail.com>
2023-02-20 20:52:16 -0500

    agent working, goes to the goal state

--- functions.py
+++ functions.py
@@ -3,7 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import gymnasium as gym
 from scipy.special import softmax
+import random
+from tqdm import tqdm
+import time
 # from gymnasium.envs.toy_text.frozen_lake import generate_random_map
+
+# Actions:
+# 0: left
+# 1: down
+# 2: right
+# 3: up
 
 
 class SARSA:
@@ -20,7 +29,7 @@ class SARSA:
         # action value matrix
         self.Qvalues = np.zeros((self.state_num, self.action_num))
 
-    def select_action(self, state, episode):  # NEED TO IMPLEMENT
+    def select_action(self, state):  # NEED TO IMPLEMENT
         """
         This function selects an action given a state in the game.
         The exploration is done using softmax (Boltmann).
@@ -29,32 +38,39 @@ class SARSA:
             state (int): current state in the game
             episode (int): current episode in the run
         """
-        if episode % 11 == 0:
-            pass  # SELECT GREEDILY - TO IMPLEMENT
+        # if episode % 11 == 0:
+        #     pass  # SELECT GREEDILY - TO IMPLEMENT
+
         action_values = self.Qvalues[state, :]
         preferences = action_values/self.temp
         preferences = softmax(preferences)
+        action = np.random.choice(a=np.arange(
+            self.action_num), p=preferences)
 
-    def simulate_episodes(self):
+        return action
+
+    def simulate_episodes(self, verbose=False):
         """
         This function simulates episodes in the frozen lake environment
         """
-        for episode in range(self.num_episodes):
+        for episode in tqdm(range(self.num_episodes)):
             # reset env
             (state, prob) = self.env.reset()
-            action = self.select_action(state, episode)
+            action = self.select_action(state)
 
-            print(f"Simulating episode {episode}.")
+            if verbose:
+                print(f"Simulating episode {episode}.")
 
             # while loop until terminal
             terminal = False
             while not terminal:
-                (next_state, reward, terminal_state, _, _) = self.env.step(action)
+                # print("Type of action: ", type(action))
+                (next_state, reward, terminal, _, _) = self.env.step(action)
 
                 # next action
-                next_action = self.select_action(next_state, episode)
+                next_action = self.select_action(next_state)
 
-                if not terminal_state:
+                if not terminal:
                     loss = reward + self.gamma * \
                         self.Qvalues[next_state, next_action] - \
                         self.Qvalues[state, action]
@@ -71,6 +87,8 @@ class SARSA:
                 if episode % 10 == 0:  # update policy for each segment
                     self.final_policy()
 
+        self.final_policy()
+
     def final_policy(self):
         """
         Obtain the final policy based on the episodes.
@@ -78,3 +96,23 @@ class SARSA:
         for i in range(self.state_num):
             self.learned_policy[i] = np.random.choice(np.where(
                 self.Qvalues[i] == np.max(self.Qvalues[i]))[0])  # may have more than 1 max
+
+
+def visualize(learned_policy, num_games):
+    for game in range(num_games):
+        env = gym.make("FrozenLake-v1", desc=None,
+                       map_name="4x4", is_slippery=False, render_mode="human")
+        (state, prob) = env.reset()
+        env.render()
+        time.sleep(2)
+
+        terminal = False
+        while not terminal:
+            if not terminal:
+                (state, reward, terminal, _, _) = env.step(
+                    int(learned_policy[state]))
+                time.sleep(1)
+            else:  # reached terminal state
+                break
+        time.sleep(1)
+    env.close()
